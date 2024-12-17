5f204d8f26130f184590997a51b9ac51a32dfbfc
CARLOSMARES <46010587+CARLOSMARES@users.noreply.github.com>
2024-12-17 02:55:50 -0600

    refactor: improve card component styles and hover effects

--- src/components/cards/targetas/targetas.jsx
+++ src/components/cards/targetas/targetas.jsx
@@ -1,98 +1,89 @@
 import { Box, Typography } from "@mui/material";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 function Targeta({ title, number, description, customStyles }) {
   return (
     <Box
       sx={{
         color: "white",
-        height: '280px',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        backgroundColor: '#800080',
-        position: 'relative',
-        borderRadius: '25px',
+        height: "280px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        backgroundColor: "#800080",
+        position: "relative",
+        borderRadius: "25px",
         ...customStyles,
-        padding: '30px 20px',
-        overflow: 'hidden',
-        transition: 'transform 0.9s ease, all 0.9s ease', // Transición para transformación suave
-        '&:hover': {
-          height: '300px',
-          transform: 'scale(1.01)', // Escala suave para agrandar el tamaño de la tarjeta sin afectar otras
-          '& .description': {
-            borderRadius: "25px",
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            zIndex: 1000,
-            position: "absolute",
-            right: "0px",
-            textAlign: "center",
-            width: "100%",
-            height: "55%",
-            bottom: "0px",
-            backgroundColor: 'white',
-            color: '#200E24',
-            transition: 'transform 0.3s ease', // Transición para la descripción
+        // padding: "30px 20px",
+        overflow: "hidden", // Evita que el hover salga del contenedor
+        transition: "transform 0.9s ease, all 0.9s ease",
+        "&:hover": {
+          height: "350px",
+          transform: "scale(1.01)",
+          transition: "transform 1.5s ease, all 1.5s ease",
+          "& .description": {
+            marginLeft: "10px",
+            marginBottom: "10px",
+            marginRight: "10px",
+            transform: "translateY(0)",
+            transition: "transform 1.5s ease",
           },
-          '& .title': {
-            transform: 'translateY(-20px)',
-            transition: 'transform 0.3s ease', // Transición para el título
+          "& .title": {
+            transform: "translateY(-20px)",
+            transition: "transform 1.5s ease",
           },
         },
       }}
     >
+      {/* Efectos de fondo */}
       <Box
         sx={{
-          position: 'absolute',
+          position: "absolute",
           bottom: 150,
           right: -150,
-          width: '250px',
-          height: '250px',
-          transition: 'all 0.3s ease',
-          background: 'radial-gradient(circle, #A018BF, #6F4CE0)',
-          borderRadius: '50%',
-          filter: 'blur(50px)',
+          width: "250px",
+          height: "250px",
+          background: "radial-gradient(circle, #A018BF, #6F4CE0)",
+          borderRadius: "50%",
+          filter: "blur(50px)",
           zIndex: 0,
         }}
       />
       <Box
         sx={{
-          position: 'absolute',
+          position: "absolute",
           top: 150,
           left: -150,
-          width: '250px',
-          height: '250px',
-          background: 'radial-gradient(circle, #A018BF, #6F4CE0)',
-          borderRadius: '50%',
-          filter: 'blur(50px)',
+          width: "250px",
+          height: "250px",
+          background: "radial-gradient(circle, #A018BF, #6F4CE0)",
+          borderRadius: "50%",
+          filter: "blur(50px)",
           zIndex: 0,
         }}
       />
 
+      {/* Contenido del título */}
       <Box
         className="title"
         sx={{
-          width: "100%",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
           gap: "10px",
           zIndex: 1,
-          transition: 'transform 0.9s ease',
-          flexWrap: 'wrap',
+          transition: "transform 0.9s ease",
         }}
       >
         <Typography
           variant="h6"
           sx={{
-            fontWeight: 'bold',
-            fontFamily: 'Poppins, sans-serif',
+            fontWeight: "bold",
+            fontFamily: "Poppins, sans-serif",
             fontSize: "16px",
             maxWidth: "50%",
-            overflowWrap: 'break-word',
-            transition: 'all 0.9s ease',
+            wordWrap: "break-word",
+            transition: "transform 0.9s ease",
           }}
         >
           {title}
@@ -101,22 +92,45 @@ function Targeta({ title, number, description, customStyles }) {
         <Typography
           variant="h6"
           sx={{
-            fontSize: '6rem',
-            fontWeight: 'bold',
-            fontFamily: 'Poppins, sans-serif',
-            transition: 'all 0.6s ease',
+            fontSize: "6rem",
+            fontWeight: "bold",
+            fontFamily: "Poppins, sans-serif",
+            transition: "transform 0.9s ease",
           }}
         >
           {number}
         </Typography>
       </Box>
 
-      <Box className="description" sx={{ zIndex: 1, borderRadius: "25px", transition: 'transform 0.9s ease' }}>
-        <Typography variant="subtitle1" sx={{
-          fontFamily: 'Poppins, sans-serif',
-          fontSize: "14px",
-          padding: "0px 25px",
-        }}>
+      {/* Descripción en hover */}
+      <Box
+        className="description"
+        sx={{
+          position: "absolute",
+          bottom: "0px",
+          left: "0px",
+          width: "calc(100% - 20px)", // Adjust width to account for margin
+          minHeight: "60%",
+          padding: "20px",
+          marginRight: "10px", // Add right margin
+          backgroundColor: "white",
+          color: "#200E24",
+          textAlign: "center",
+          borderRadius: "25px 25px 0 0", // Round only the top corners
+          transform: "translateY(100%)", // Initially hide
+          transition: "transform 1.5s ease",
+          zIndex: 1,
+          overflowWrap: "break-word", // Prevent text overflow
+          boxSizing: "border-box", // Include padding and border in the element's total width and height
+        }}
+      >
+        <Typography
+          variant="subtitle1"
+          sx={{
+            fontFamily: "Poppins, sans-serif",
+            fontSize: "14px",
+          }}
+        >
           {description}
         </Typography>
       </Box>
